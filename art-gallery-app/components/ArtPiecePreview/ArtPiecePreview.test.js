b29271bd14e090f580f1137880f02a583483139f
Ron McCoy <rongoods@gmail.com>
2023-11-16 10:18:28 +0100

    complete test for artist name in spotlight component

--- art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.test.js
+++ art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import ArtPiecePreview from "./ArtPiecePreview";
 
 test("renders an Art Piece title", () => {
--- art-gallery-app/components/Spotlight/Spotlight.js
+++ art-gallery-app/components/Spotlight/Spotlight.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Spotlight({ image, artist }) {
+export default function Spotlight({ image, artist, name }) {
   return (
     <div>
-      <Image src={image} width={600} height={400}></Image>
+      <Image src={image} width={600} height={400} name={name}></Image>
       <h2>{artist}</h2>
     </div>
   );
